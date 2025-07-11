ed1203f8f3ccf1d5e72d7108fd54120e9b896850
Refilwe15 <scottfrankie864@gmail.com>
2025-07-11 14:20:05 +0200

    second commit

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-// Select all components
-const infoBox = document.getElementById('infoBox');
 
-document.querySelector('.cpu').addEventListener('click', () => {
-    infoBox.innerHTML = "CPU : It's the brain of the computer where most calculations take place.";
+const components = [
+    {
+        box: 'cpuBox', modal: 'cpuModal'
+    },
+    {
+        box: 'cmosBox', modal: 'cmosModal'
+    },
+    {
+        box: 'storageBox', modal: 'storageModal'
+    },
+    {
+        box: 'romBox', modal: 'romModal'
+    },
+    {
+        box: 'ramBox', modal: 'ramModal'
+    }
+];
+
+
+components.forEach(component => {
+    const box = document.getElementById(component.box);
+    const modal = document.getElementById(component.modal);
+    const closeBtn = modal.querySelector('.close-btn');
+
+    box.addEventListener('click', () => {
+        modal.style.display = 'block';
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
 });
 
-document.querySelector('.cmos').addEventListener('click', () => {
-    infoBox.innerHTML = "CMOS : It's a battery-powered chip that stores BIOS settings.";
-});
 
-document.querySelector('.storage').addEventListener('click', () => {
-    infoBox.innerHTML = "Storage Device: It stores all data permanently, including the operating system, apps, and files.";
-});
-
-document.querySelector('.rom').addEventListener('click', () => {
-    infoBox.innerHTML = "ROM : It contains the firmware or BIOS, essential for booting the computer.";
-});
-
-document.querySelector('.ram').addEventListener('click', () => {
-    infoBox.innerHTML = "RAM: It's temporary memory used by the computer to store data for running programs.";
-});
